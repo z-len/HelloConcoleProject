ed04332995839cc78ad4da2f78cc56cdb4aaa902
Elena Zosina <z-len@yandex.ru>
2023-01-12 00:04:59 +0300

    add Example002_HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿//Console.WriteLine("Введите Ваше имя:");
+// string username = Console.ReadLine();
+//Console.WriteLine("Привет, ");
+//Console.WriteLine(username);
+
+Console.Write("Введите Ваше имя:");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
