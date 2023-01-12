d21d3f19b5c350383ad789919200659e65457bca
Elena Zosina <z-len@yandex.ru>
2023-01-12 09:20:40 +0300

    add if and else

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "лена")
+{
+    Console.Write("Здравствуй, Леночка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
